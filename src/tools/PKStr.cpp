c109c5a250761be1e4dc7aa44552c09512fbf603
Damien <roy204@gmail.com>
2016-08-17 16:51:27 +0200

    - Fixing bug

--- src/tools/PKStr.cpp
+++ src/tools/PKStr.cpp
@@ -235,12 +235,12 @@ std::string PKStr::replaceString(std::string find,
 
 std::string PKStr::escapeSingleQuotes(std::string input)
 {
-	return PKStr::replaceString(input, "'", "\\'");
+	return PKStr::replaceString("'", "\\'", input);
 }
 
 std::wstring PKStr::escapeSingleQuotes(std::wstring input)
 {
-	return PKStr::replaceString(input, L"'", L"\\'");
+	return PKStr::replaceString(L"'", L"\\'", input);
 }
 
 void PKStr::explode(std::wstring str,
